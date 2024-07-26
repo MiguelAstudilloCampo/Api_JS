c8b7758c7279672488fa9251b4cc1cd76b8d6398
MiguelAstudilloCampo <miguelangelastudillo38@gmail.com>
2024-07-25 20:25:43 -0500

    Se agregaron los cors

--- src/index.js
+++ src/index.js
@@ -1,27 +1,36 @@
 const express = require("express");
 const mongoose = require("mongoose");
 require("dotenv").config();
-const userRoutes = require("./routes/user")
+const userRoutes = require("./routes/user");
 
 const app = express();
 const port = process.env.PORT || 4000;
 
 // middlewre
 
-app.use(express.json())
-app.use("/api",userRoutes);
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
+app.use(express.json());
+app.use("/api", userRoutes);
 
 // routes
 
-app.get ("/",(req, res) => {
-    res.send("¡Bienvenidos!")
+app.get("/", (req, res) => {
+  res.send("¡Bienvenidos!");
 });
 
 //mongodb conection
 
-mongoose.connect(
-    process.env.MONGODB_URI)
-    .then(()=>console.log("Connected to MongoDB Atlas"))
-    .catch((error)=>console.error(error))
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => console.log("Connected to MongoDB Atlas"))
+  .catch((error) => console.error(error));
 
-app.listen(port, () => console.log ("server listening on port", port));
+app.listen(port, () => console.log("server listening on port", port));
